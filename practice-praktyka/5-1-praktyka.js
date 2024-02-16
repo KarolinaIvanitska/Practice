c54cc2fb9d9e941c7c4a465e23ef081a8479c2b6
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-02-16 12:04:43 +0100

    Update

--- practice-praktyka/5-1-praktyka.js
+++ practice-praktyka/5-1-praktyka.js
@@ -55,31 +55,31 @@
 //showProduct(product) - коллбек приймаючий об'єкт
 //продукту і логірующий їх у консоль
 
-setInterval(() => {
-  console.log(Date.now());
- }, 1000);
+// setInterval(() => {
+//   console.log(Date.now());
+//  }, 1000);
 
-function generateUniqueId() {
-  const timestamp = Date.now();
-  const uniqueId = `${timestamp}${Math.floor(Math.random() * 10000)}`;
-    return uniqueId;
-}
-console.log(new Date().getTime());
-function makeProduct(name, price, callback) {
-    const id = generateUniqueId();
-    const product = {
-        id,
-        name,
-        price,
-    };
-    return callback(product);
-}
+// function generateUniqueId() {
+//   const timestamp = Date.now();
+//   const uniqueId = `${timestamp}${Math.floor(Math.random() * 10000)}`;
+//     return uniqueId;
+// }
+// console.log(new Date().getTime());
+// function makeProduct(name, price, callback) {
+//     const id = generateUniqueId();
+//     const product = {
+//         id,
+//         name,
+//         price,
+//     };
+//     return callback(product);
+// }
 
-const showProduct = product => `id: ${product.id}, Product: ${product.name}, price: ${product.price}`;
+// const showProduct = product => `id: ${product.id}, Product: ${product.name}, price: ${product.price}`;
 
-console.log(makeProduct('Banana', 300, showProduct));
-console.log(makeProduct('Apple', 100, showProduct));
-console.log(makeProduct('Orange', 250, showProduct));
+// console.log(makeProduct('Banana', 300, showProduct));
+// console.log(makeProduct('Apple', 100, showProduct));
+// console.log(makeProduct('Orange', 250, showProduct));
 
 //TODO:============task-04=========================
 //Напишіть функцію each(array, callback), яка
@@ -89,3 +89,13 @@ console.log(makeProduct('Orange', 250, showProduct));
 //якого будуть результати виклику callback
 //callback функції повинна множити елементи на 2
 //! Використай анонімну колбєк функцію
+
+const numbers = [1, 2, 4]
+const each = (array, callback) => {
+    const Newarray = []; 
+    array.forEach ((number) => {
+        newArray.push(callback(number))
+    });
+    return newArray;
+}
+console.log(each(numbers, (number) => number * 2));
